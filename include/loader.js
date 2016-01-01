9628219f1dc6a0468db528432cca8d1217ad6df1
SirPL <ciemakat@gmail.com>
2016-01-01 17:38:14 +0100

    Loader fix
    Loader has been writen from a scratch;
    Footnotes links has been fixed
    Some of home sub-sites has been moved

--- include/loader.js
+++ include/loader.js
@@ -1,100 +1,121 @@
-$.routes({'/': function(){$('.wrap-main').load('sites/home.html',function(response ,status,
-                                                         xhr) {
-            if(status == "error") {
-                $('.wrap-main').load("404.html");
+var lastVisitedURL = {"folder": "home", "name": ""};
+
+$.routes({
+    "/": loadHome,
+    "/home": loadHome,
+
+    "/:folder/": function(param){
+        $.ajax({
+            type: "POST",
+            url: "/sites/"+param.folder+"/home.html",
+            success: function(data){
+                $("#content").html(data);
+                loadAsideMenu(param);
+            },
+            error: function() {
+                $("#content").load("/404.html");
+                colorize();
+            },
+            complete: function() {
+                lastVisitedURL = param;
             }
         });
-    $.ajax({
-        url: 'sites/' + params.folder +
-        '/aside_menu.html',
-        success: function(result){
-            $('.wrap-aside' ).html(result);
-            colorize();
-        },
-        error: function(xhr, status, error) {
+    },
+
+    "/:folder/:file": function(param){
+        $.ajax({
+            type: "POST",
+            url: "/sites/"+param.folder+"/"+param.file+".html",
+            success: function(data){
+                $("#content").html(data);
+                loadAsideMenu(param);
+            },
+            error: function() {
+                $("#content").load("/404.html");
+                colorize();
+            },
+            complete: function() {
+                lastVisitedURL = param;
+            }
+        });
+    },
+
+    "/:folder/:file/:anchor": function(param){
+        if(lastVisitedURL.folder != param.folder ||
+            lastVisitedURL.file != param.file) {
             $.ajax({
-                url: 'aside_menu.html',
-                success: function(result){
-                    $('.wrap-aside' ).html(result);
+                type: "POST",
+                url: "/sites/"+param.folder+"/"+param.file+".html",
+                success: function(data){
+                    $("#content").html(data);
+                    loadAsideMenu(param);
+                },
+                error: function() {
+                    $("#content").load("/404.html");
                     colorize();
                 },
-                async: false
+                complete: function() {
+                    lastVisitedURL = param;
+                }
             });
-        },
-        async: false
-    });
-    },
-    '/:name': function(params){
-        $('.wrap-main').load('sites/' + params.name + '.html', function(response,
-                                                                        status,
-                                                                        xhr) {
-            if(status == "error") {
-                $('.wrap-main').load("404.html");
-            }
-        });
-        $.ajax({
-            url: 'sites/' + params.folder +
-            '/aside_menu.html',
-            success: function(result){
-                $('.wrap-aside' ).html(result);
-                colorize();
-            },
-            error: function(xhr, status, error) {
-                $.ajax({
-                    url: 'aside_menu.html',
-                    success: function(result){
-                        $('.wrap-aside' ).html(result);
-                        colorize();
-                    },
-                    async: false
-                });
-            },
-            async: false
-        });
-    },
-    '/:folder/:name': function(params){
-        $('.wrap-main').load('sites/' + params.folder +
-            '/' + params.name + '.html', function(response,
-                                                  status,
-                                                  xhr) {
-            if(status == "error") {
-                $('.wrap-main').load("404.html");
-            }
-        });
-        $.ajax({
-            url: 'sites/' + params.folder +
-            '/aside_menu.html',
-            success: function(result){
-                $('.wrap-aside' ).html(result);
-                colorize();
-            },
-            error: function(xhr, status, error) {
-                $.ajax({
-                    url: 'aside_menu.html',
-                    success: function(result){
-                        $('.wrap-aside' ).html(result);
-                        colorize();
-                    },
-                    async: false
-                });
-            },
-            async: false
-        });
-    },
-    '/:folder/:name/:anchor': function(params){
-        $('.wrap-main').load('sites/' + params.folder +
-            '/' + params.name + '.html', function(response,
-                                                  status,
-                                                  xhr) {
-            if(status == "error") {
-                $('.wrap-main').load("404.html");
-            } else {
-                $(window).scrollTop($('#' + params.anchor).offset().top);
-            }
-        });
+        }
+        $(window).scrollTop($('#' + param.anchor).offset().top);
     }
 });
 
+
+function loadHome(){
+
+    $.ajax({
+        type: "POST",
+        url: "/sites/home/home.html",
+        success: function(data){
+            $("#content").html(data);
+            loadAsideMenu({"folder": "/"});
+        },
+        error: function() {
+            $("#content").load("/404.html");
+            colorize();
+        },
+        complete: function() {
+            lastVisitedURL = {"folder": "/home/", "name": ""};
+        }
+    });
+}
+
+function loadAsideMenu(param) {
+    if(param.folder != lastVisitedURL.folder) {
+        $.ajax({
+            method: "POST",
+            url: function () {
+                if (param.folder == "/") {
+                    console.log("sites/home/aside_menu.html");
+                    return "sites/home/aside_menu.html";
+                }
+                else {
+                    console.log("sites/" + param.folder + "/aside_menu.html");
+                    return "sites/" + param.folder + "/aside_menu.html";
+                }
+            }(),
+            success: function (data) {
+                $(".wrap-aside").html(data);
+            },
+            complete: function(){
+                colorize();
+            }
+        });
+    } else {
+        colorize();
+    }
+
+
+}
+
 function colorize() {
+    removeColor();
     $('.wrap-aside > ul a[href=' + window.location.hash + ']').addClass("current-page");
+}
+
+function removeColor() {
+    $('.wrap-aside > ul a').removeClass("current-page");
 }
